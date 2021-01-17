dd16a71b6fc85831338c4a8e96616b5cc4258b45
Natasha Anderson <andersonnatashaa@gmail.com>
2021-01-17 00:24:08 +0000

    homepage: add image, link to /cards

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -3,11 +3,13 @@
 function Homepage() {
   return (
     <div>
-      <h3 class="homepage-text">Welcome!</h3>
-      <img id="balloonicorn-img" src="/static/img/balloonicorn.jpg"></img>
-      <a class="homepage-text href="/cards">Go to cards</a>
+    <h3> Hi there, thanks for showing up! You can find all the cards <a href="/cards">here!</a> </h3>
+    <img src="/static/img/balloonicorn.jpg"></img>
     </div>
-  );
+
+);
 }
+
+
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
