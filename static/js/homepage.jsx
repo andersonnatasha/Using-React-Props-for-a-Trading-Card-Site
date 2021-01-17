85e56367516036284935d57c92e2798f66f9c653
Natasha Anderson <andersonnatashaa@gmail.com>
2021-01-17 00:36:08 +0000

    center homepage image

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -4,7 +4,7 @@ function Homepage() {
   return (
     <div>
     <h3> Hi there, thanks for showing up! You can find all the cards <a href="/cards">here!</a> </h3>
-    <img src="/static/img/balloonicorn.jpg"></img>
+    <img id="homepage-balloonicorn" src="/static/img/balloonicorn.jpg"></img>
     </div>
 
 );
