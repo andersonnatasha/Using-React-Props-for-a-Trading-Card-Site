8f842435237f11e0c3cca85d09e0890255ebd2f9
Natasha Anderson <andersonnatashaa@gmail.com>
2021-01-17 01:09:41 +0000

    add object of card data and remove hard coded cards:

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -4,8 +4,52 @@ const tradingCardData = [
     name: 'Balloonicorn',
     skill: 'video games',
     imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpg'
+  },
+
+  {
+    name: 'Seed.py',
+    skill: 'making curry dishes',
+    imgUrl: '/static/img/seedpy.jpg'
+  },
+
+  {
+    name: 'Polymorphism',
+    skill: 'costumes',
+    imgUrl: '/static/img/polymorphism.jpg'
+  },
+
+  {
+    name: 'Short Stack Overflow',
+    skill: 'ocean animal trivia',
+    imgUrl: '/static/img/shortstack-overflow.jpg'
+  },
+
+  {
+    name: 'Merge',
+    skill: 'bullet journaling',
+    imgUrl: '/static/img/merge.jpg'
   }
-]
+];
+
 
 function TradingCard(props) {
   return (
