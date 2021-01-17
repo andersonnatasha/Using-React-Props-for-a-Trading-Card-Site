cba9c718fb9c81b67c2cf0f4f66b328040cfac31
Natasha Anderson <andersonnatashaa@gmail.com>
2021-01-17 00:02:00 +0000

    inital commit

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -3,9 +3,9 @@
 function Homepage() {
   return (
     <div>
-      <h3>Welcome!</h3>
-      <a href="/cards">Go to cards</a>
+      <h3 class="homepage-text">Welcome!</h3>
       <img id="balloonicorn-img" src="/static/img/balloonicorn.jpg"></img>
+      <a class="homepage-text href="/cards">Go to cards</a>
     </div>
   );
 }
