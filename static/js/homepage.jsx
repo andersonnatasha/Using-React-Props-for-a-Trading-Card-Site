3e0e14765f6e22c9622880110ab501491eb3d1c5
Natasha Anderson <andersonnatashaa@gmail.com>
2021-01-17 00:40:44 +0000

    change image tag to be self closing

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -2,10 +2,10 @@
 
 function Homepage() {
   return (
-    <div>
+    <React.Fragment>
     <h3> Hi there, thanks for showing up! You can find all the cards <a href="/cards">here!</a> </h3>
-    <img id="homepage-balloonicorn" src="/static/img/balloonicorn.jpg"></img>
-    </div>
+    <img id="homepage-balloonicorn" src="/static/img/balloonicorn.jpg"/>
+    </React.Fragment>
 
 );
 }
